7704e75c5223982a5ed207ef1e43eb77ea5ae31c
PadoleKiran <124488404+PadoleKiran@users.noreply.github.com>
2025-10-04 00:42:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-function firstChar(text) {
-  // your code here
-	let v = text.trim();
-	if(v.length == 0){
-		// console.log("");
-		return "";
-	}
-		 // console.log(v[0]);
-		return v[0];
+// function firstChar(text) {
+//   // your code here
+// 	let v = text.trim();
+// 	if(v.length == 0){
+// 		// console.log("");
+// 		return "";
+// 	}
+// 		 // console.log(v[0]);
+// 		return v[0];
 	
+// }
+
+// // Do not change the code below
+// //Uncomment the following line to show the prompt popup
+// //const text = prompt("Enter text:");
+// alert(firstChar(text));
+
+
+function firstChar(text) {
+  // Remove leading and trailing spaces
+  let v = text.trim();
+
+  // If empty after trimming, return empty string
+  if (v.length === 0) {
+    return "";
+  }
+
+  // Otherwise return first character
+  return v[0];
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// Uncomment this line when running in browser:
+const text = prompt("Enter text:");
 alert(firstChar(text));
+
